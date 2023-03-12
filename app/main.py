c94325fa2f4afb2ff5181f5a2445c593a19e5a38
EndyB <endyb.dev@gmail.com>
2023-03-12 11:23:15 +1100

    Fix output tokens and cleaning data to transform to json format

--- app/main.py
+++ app/main.py
@@ -7,10 +7,8 @@ app = FastAPI()
 
 @app.get('/')
 def home():
-    prompt = "Crea una malla curricular de cursos para estas competencias corporativas y organízala por niveles de cargo: Trabajo en equipo, ofimática y liderazgo. Escribe el resultado en notación JSON"
-    res = ai_response(prompt)
-    print(type(res))
-    json_res = json.loads(str(res))
-    response = json_res['choices'][0]['text']
-    #print(type(response))
+    prompt = """Crea una malla curricular de cursos para estas competencias corporativas 
+                y organízala por niveles de cargo: Trabajo en equipo, ofimática y liderazgo. 
+                Escribe el resultado en notación JSON"""
+    response = ai_response(prompt)
     return {'t_learning_ai_response': response}
--- app/utils/scripts.py
+++ app/utils/scripts.py
@@ -1,3 +1,5 @@
+import json
+
 import openai
 
 from app.core.config import openai_apikey
@@ -5,14 +7,23 @@ from app.core.config import openai_apikey
 
 openai.api_key = openai_apikey
 
-def ai_response(prompt: str = 'Qué servicios ofrece T-learning'): 
+def ai_response(
+    prompt: str = 'Qué servicios ofrece T-learning'
+): 
     response = openai.Completion.create(
         model="text-davinci-003",
         prompt= prompt,
-        temperature=0.7,
-        max_tokens=256,
+        temperature=0.2,
+        max_tokens=768,
         top_p=1,
         frequency_penalty=0,
         presence_penalty=0
     )
-    return response
+    response = str(response)
+    res = response#.replace("'\n", "")
+    json_res = json.loads(res)
+    text = json_res['choices'][0]['text']
+    print(text)
+    text = str(text).replace('\n', '').replace('.', '').replace('\\', '')
+    json_text = json.loads(text)
+    return json_text
