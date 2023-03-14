5aa0e53196d935dbaa58a045b1c9c56500f1c0d1
EndyB <endyb.dev@gmail.com>
2023-03-14 20:08:56 +1100

    Custom prompt for test

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 import json
 
 from fastapi import FastAPI
+from fastapi import Query
+
 from app.utils.scripts import ai_response
+
+
 
 app = FastAPI()
 
@@ -12,3 +16,13 @@ def home():
                 Escribe el resultado en notación JSON"""
     response = ai_response(prompt)
     return {'t_learning_ai_response': response}
+
+
+@app.get('/ai')
+def get_custom_query(
+    prompt: str = Query(..., example="Dame un json con canciones vallenatas")
+):
+    response = ai_response(prompt)
+    return {'t_learning_ai_response': response}
+
+
