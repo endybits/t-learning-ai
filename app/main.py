31e068bf592c8709d37292a427c401bbf046fbf0
EndyB <endyb.dev@gmail.com>
2023-03-14 19:38:10 +1100

    Adding prompt

--- app/main.py
+++ app/main.py
@@ -10,5 +10,5 @@ def home():
     prompt = """Crea una malla curricular de cursos para estas competencias corporativas 
                 y organízala por niveles de cargo: Trabajo en equipo, ofimática y liderazgo. 
                 Escribe el resultado en notación JSON"""
-    response = ai_response()
+    response = ai_response(prompt)
     return {'t_learning_ai_response': response}
